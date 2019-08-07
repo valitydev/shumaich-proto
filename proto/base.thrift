7445ef9a359815e7e9328671b5909f582b7d5ee1
Pavel Popov <tolkonepiu@users.noreply.github.com>
2019-08-07 17:09:34 +0300

    Initial proto (#1)
    * Initial proto
    
    * Add build_utils
    
    * Update shumpune.thrift
    
    * Review fixes

---
+++ proto/base.thrift
@@ -0,0 +1,27 @@
+namespace java com.rbkmoney.damsel.shumpune.base
+namespace erlang base
+
+/** Символьный код, уникально идентифицирующий валюту. */
+typedef string CurrencySymbolicCode
+
+/**
+ * Отметка во времени согласно RFC 3339.
+ *
+ * Строка должна содержать дату и время в UTC в следующем формате:
+ * `2016-03-22T06:12:27Z`.
+ */
+typedef string Timestamp
+
+/** Сумма в минимальных денежных единицах. */
+typedef i64 Amount
+
+/** Непрозрачный для участника общения набор данных */
+typedef binary Opaque
+
+/**
+ * Исключение, сигнализирующее о непригодных с точки зрения бизнес-логики входных данных
+ */
+exception InvalidRequest {
+    /** Список пригодных для восприятия человеком ошибок во входных данных */
+    1: required list<string> errors
+}
